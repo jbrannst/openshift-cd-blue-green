d6086f0d5940d6d3c122884786a84a4d02b0177c
Johannes Brännström <jbrannst@redhat.com>
2017-06-23 12:30:52 +0200

    use a prod project

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 oc new-project jb-dev --display-name="Tasks - Dev"
-oc new-project jb-stage --display-name="Tasks - Stage"
+oc new-project jb-prod --display-name="Tasks - Prod"
 oc new-project jb-cicd --display-name="CI/CD"
-oc create -f jenkins-ephemeral.yaml -n jb-cicd
-oc process -f cicd-template.yaml -p DEV_PROJECT=jb-dev -p STAGE_PROJECT=jb-stage | oc create -f - -n jb-cicd
+oc process -f jenkins-ephemeral.yaml | oc create -f -  -n jb-cicd
+oc process -f cicd-template.yaml -p DEV_PROJECT=jb-dev -p PROD_PROJECT=jb-prod | oc create -f - -n jb-cicd
+oc policy add-role-to-user edit system:serviceaccount:jb-cicd:jenkins -n jb-dev
+oc policy add-role-to-user edit system:serviceaccount:jb-cicd:jenkins -n jb-prod
+oc policy add-role-to-user view system:serviceaccount:jb-dev:default -n jb-dev
+oc policy add-role-to-user view system:serviceaccount:jb-prod:default -n jb-prod
