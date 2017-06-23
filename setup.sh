2a2c7fc85107e8288b62daa9cd269d9db9c1a155
Johannes Brännström <jbrannst@redhat.com>
2017-06-24 00:33:51 +0200

    parameter for project prefix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-oc new-project jb-dev --display-name="Tasks - Dev"
-oc new-project jb-prod --display-name="Tasks - Prod"
-oc new-project jb-cicd --display-name="CI/CD"
-oc process -f jenkins-ephemeral.yaml | oc create -f -  -n jb-cicd
-oc process -f cicd-template.yaml -p DEV_PROJECT=jb-dev -p PROD_PROJECT=jb-prod | oc create -f - -n jb-cicd
-oc policy add-role-to-user edit system:serviceaccount:jb-cicd:jenkins -n jb-dev
-oc policy add-role-to-user edit system:serviceaccount:jb-cicd:jenkins -n jb-prod
-oc policy add-role-to-user view system:serviceaccount:jb-dev:default -n jb-dev
-oc policy add-role-to-user view system:serviceaccount:jb-prod:default -n jb-prod
+pre=jb
+oc new-project ${pre}-dev --display-name="Tasks - Dev"
+oc new-project ${pre}-prod --display-name="Tasks - Prod"
+oc new-project ${pre}-cicd --display-name="CI/CD"
+oc process -f jenkins-ephemeral.yaml | oc create -f -  -n ${pre}-cicd
+oc process -f cicd-template.yaml -p DEV_PROJECT=${pre}-dev -p PROD_PROJECT=${pre}-prod | oc create -f - -n ${pre}-cicd
+oc policy add-role-to-user edit system:serviceaccount:${pre}-cicd:jenkins -n ${pre}-dev
+oc policy add-role-to-user edit system:serviceaccount:${pre}-cicd:jenkins -n ${pre}-prod
+oc policy add-role-to-user view system:serviceaccount:${pre}-dev:default -n ${pre}-dev
+oc policy add-role-to-user view system:serviceaccount:${pre}-prod:default -n ${pre}-prod
