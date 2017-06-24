438cb05a72abf6bfcefd9dfde24bd81e04e4c16e
Johannes Brännström <jbrannst@redhat.com>
2017-06-25 00:49:13 +0200

    clear parameter naming

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
-pre=jb
-oc new-project ${pre}-test --display-name="Tasks - Test"
-oc new-project ${pre}-prod --display-name="Tasks - Prod"
-oc new-project ${pre}-cicd --display-name="CI/CD"
-#oc process -f jenkins-ephemeral.yaml | oc create -f -  -n ${pre}-cicd
-oc process -f cicd-template.yaml -p TEST_PROJECT=${pre}-test -p PROD_PROJECT=${pre}-prod | oc create -f - -n ${pre}-cicd
+cicd=jb-cicd
+test=jb-test
+prod=jb-prod
+oc new-project ${test} --display-name="Kitchensink - Test"
+oc new-project ${prod} --display-name="Kitchensink - Prod"
+oc new-project ${cicd} --display-name="CI/CD"
+oc process -f cicd-template.yaml -p TEST_PROJECT=${test} -p PROD_PROJECT=${prod} | oc create -f - -n ${cicd}
 oc set resources dc/jenkins --limits=memory=1Gi
-oc policy add-role-to-user edit system:serviceaccount:${pre}-cicd:jenkins -n ${pre}-test
-oc policy add-role-to-user edit system:serviceaccount:${pre}-cicd:jenkins -n ${pre}-prod
-oc policy add-role-to-user view system:serviceaccount:${pre}-test:default -n ${pre}-test
-oc policy add-role-to-user view system:serviceaccount:${pre}-prod:default -n ${pre}-prod
+oc policy add-role-to-user edit system:serviceaccount:${cicd}:jenkins -n ${test}
+oc policy add-role-to-user edit system:serviceaccount:${cicd}:jenkins -n ${prod}
+oc policy add-role-to-user view system:serviceaccount:${test}:default -n ${test}
+oc policy add-role-to-user view system:serviceaccount:${prod}:default -n ${prod}
