e28bc85c918ee755697ec4c7fad1c8b3370b1a58
Johannes Brännström <jbrannst@redhat.com>
2017-09-21 15:09:52 +0200

    better variable naming

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 cicd=cicd
 test=test
 prod=prod
-git-repo=https://github.com/jbrannst/appdev-foundations-kitchensink
+git_repo=https://github.com/jbrannst/appdev-foundations-kitchensink
 date
 echo "Creating namespaces."
 oc new-project ${test} --display-name="Kitchensink - Test" > /dev/null
 oc new-project ${prod} --display-name="Kitchensink - Prod" > /dev/null
 oc new-project ${cicd} --display-name="CI/CD" > /dev/null
 echo "Creating components."
-oc process -f cicd-template.yaml -p TEST_PROJECT=${test} -p PROD_PROJECT=${prod} -p GIT_REPO=${git-repo} | oc create -f - -n ${cicd}
+oc process -f cicd-template.yaml -p TEST_PROJECT=${test} -p PROD_PROJECT=${prod} -p GIT_REPO=${git_repo} | oc create -f - -n ${cicd}
 oc set resources dc/jenkins --limits=memory=1Gi
 oc policy add-role-to-user edit system:serviceaccount:${cicd}:jenkins -n ${test}
 oc policy add-role-to-user edit system:serviceaccount:${cicd}:jenkins -n ${prod}
